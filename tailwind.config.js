6c3dff926f1bb8ece4b6e5f710bd46f17214c397
Monika <130761795+monikabisht343@users.noreply.github.com>
2024-04-22 01:42:42 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+// tailwind.config.js
+module.exports = {
+  content: ['./src/**/*.{js,css}'],
+  theme: {
+      extend: {
+          colors: {
+              customColor1: '#ff5722',
+              customColor2: '#4caf50',
+              // Add more custom colors as needed
+          },
+      },
+  },
+  plugins: [],
+};
